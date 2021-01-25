f890c0fffd7b181a95afe8af42979b3e57805219
Talha Jubayer <jubayertalha194@gmail.com>
2021-01-26 03:39:59 +0600

    add test project 1

---
+++ Practice/Test1/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            string name = "Talha";
+
+            for (i = 0; i < 10; i++) 
+                if(i%2==1)
+                    Console.WriteLine("Hello "+name+" "+i);
+
+            Console.ReadLine();
+        }
+    }
+}
