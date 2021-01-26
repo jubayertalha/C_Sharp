0bba426590b1e58193eeb9900b758f105f4d4e90
Talha Jubayer <jubayertalha194@gmail.com>
2021-01-27 00:40:38 +0600

    add test wpf app project 1

---
+++ Practice/Test1/WpfApplication1/WpfApplication1/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string name = "";
+        string xtr = "                              ";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            name = t1.Text;
+
+            if (r1.IsChecked == true)
+                MessageBox.Show("Hello "+name+"!"+xtr);
+            else
+                MessageBox.Show("Good Bye "+name+"!"+xtr);
+        }
+
+    }
+}
