9a3b03f4cf5428d8a4903542153e01ee0f5a1f2c
Talha Jubayer <jubayertalha194@gmail.com>
2021-03-01 10:59:29 +0600

    add lab 5 association

---
+++ Llab_6_Association/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Department d1 = new Department("CSE");
+            Department d2 = new Department("EEE");
+
+            Student s1 = new Student("Talha", "19-41468-3", 3.90);
+            Student s2 = new Student("Jubayer", "19-4143-3", 3.60);
+            Student s3 = new Student("Rahman", "19-41439-3", 3.30);
+            Student s4 = new Student("Sheikh", "19-41426-3", 2.70);
+
+            d1.AddStudent(s1, s2);
+            d2.AddStudent(s3);
+            d2.AddStudent(s4);
+
+            d1.PrintStudent();
+            d2.PrintStudent();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Llab_6_Association/ConsoleApplication1/ConsoleApplication1/Student.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Student
+    {
+        string name;
+        string id;
+        double cgpa;
+        Department dept;
+
+        public Department Dept
+        {
+            get { return dept; }
+            set { dept = value; }
+        }
+
+        public Student(string name, string id, double cgpa)
+        {
+            this.name = name;
+            this.id = id;
+            this.cgpa = cgpa;
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine("****************");
+            Console.WriteLine("Student Name: " + name);
+            Console.WriteLine("Student ID: " + id);
+            Console.WriteLine("Student Department: " + dept.Name);
+            Console.WriteLine("***************");
+        }
+    }
+}
