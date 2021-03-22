176bb13fe95e023a26c4c13095818df54210b890
Talha Jubayer <jubayertalha194@gmail.com>
2021-03-22 10:18:08 +0600

    lab 2 final student info initial

---
+++ OOP2_Sp21_Final/Lab_2_Association/StudentInfo/StudentInfo/Department.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentInfo
+{
+    class Department
+    {
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        Student[] students;
+        int studentCount;
+
+        public Department(string name)
+        {
+            this.name = name;
+            students = new Student[50];
+        }
+
+        public void AddStudent(params Student[] stu)
+        {
+            foreach (Student s in stu)
+            {
+                students[studentCount++] = s;
+                s.Dept = this;
+            }
+        }
+
+        public void PrintStudent()
+        {
+            foreach (Student s in students)
+                if (s != null)
+                    s.ShowInfo();
+        }
+    }
+}
